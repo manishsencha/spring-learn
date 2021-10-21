fe83a9716420e3ccc67a34d67eaaf5a25df00248
Manish Sencha <manishsencha2001@gmail.com>
2021-10-21 08:02:02 +0530

    Consumed API

--- src/main/java/com/learn/learn/LearnApplication.java
+++ src/main/java/com/learn/learn/LearnApplication.java
@@ -1,11 +1,32 @@
 package com.learn.learn;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LearnApplication {
+	private static final Logger log = LoggerFactory.getLogger(LearnApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(LearnApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception{
+		return args -> {
+			Quote quote = restTemplate.getForObject("https://quoters.apps.pcfone.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
 	}
 }
